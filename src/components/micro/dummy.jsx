8773b1c11dde6e865beb7f8faa1a4feda2b71cd9
Shiyam Sarker <78273465+shiyam-sarker10@users.noreply.github.com>
2024-05-19 19:28:49 +0600

    modification

---
+++ src/components/micro/dummy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const dummy = () => {
+    return (
+        <div>
+             This is a dummy of micro component to tell that you have put micro components of your project in micro folder . feel free to modify the structure as you want
+        </div>
+    );
+};
+
+export default dummy;
